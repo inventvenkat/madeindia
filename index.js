d8ef1b51df2b145df9486386da680bb5c77480e0
= <=>
2016-08-19 14:59:59 +0530

    check 1

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
-
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(path.join(__dirname + '/views/pages/index.html'));
 });
 
 app.get('/cool', function(request, response) {
